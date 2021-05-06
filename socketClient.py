76d216f74376d5088781586bc788c4d34b884a09
Mike Shafter <yuhang.061@gmail.com>
2021-05-06 09:24:46 +0800

    Update socketClient.py

--- socketClient.py
+++ socketClient.py
@@ -5,6 +5,9 @@ my_socket = socket.socket()
 address = input('Enter IPv4 address of server: ')
 port = int(input('Enter port number of server: '))
 my_socket.connect((address, port))
-print(my_socket.recv(1024))
-my_socket.close()
 
+data = b''
+while b'\n' not in data:
+    data += my_socket.recv(1024)
+print(data)
+my_socket.close()
