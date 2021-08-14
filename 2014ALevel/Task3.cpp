1487d0ac376579199cf4f4e48dd8fff7d2c89c82
Mineshafter61 <yuhang.061@gmail.com>
2021-08-14 12:05:32 +0900

    yes

---
+++ 2014ALevel/Task3.cpp
@@ -0,0 +1,113 @@
+// Task 3.1
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+  int choice = 0;
+
+  LinkedList linkedList;
+
+  while (choice != 5) {
+    cout << "1. Add an item" << endl;
+    cout << "2. Traverse the linked list of used nodes and output the data values" << endl;
+    cout << "3. Output all pointers and data values" << endl;
+    cout << "5. Exit" << endl;
+
+    cout << "Choice: " << endl;
+    cin >> choice;
+
+
+    // Task 3.3
+
+    switch (choice) {
+      case 1:
+        linkedList.AddNode();
+        break;
+
+      case 2:
+        linkedList.Traversal();
+        break;
+
+      case 3:
+        linkedList.DisplayLinkedList();
+        break;
+
+      case 4:
+        linkedList.ReverseTraversal();
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return 0;
+}
+
+
+// Task 3.2
+
+class ListNode {
+private:
+  string DataValue;
+  int PointerValue;
+
+public:
+  ListNode (string data, int ptr) {
+    DataValue = data;
+    PointerValue = ptr;
+  }
+
+  int GetPointerValue () {
+    return PointerValue;
+  }
+
+  string GetDataValue () {
+    return DataValue;
+  }
+
+  void SetPointerValue (int ptr) {
+    PointerValue = ptr;
+  }
+
+  void SetDataValue (string data) {
+    DataValue = data;
+  }
+
+};
+
+
+class LinkedList {
+private:
+  ListNode Node[31];
+  int Start;
+  int NextFree;
+
+public:
+  LinkedList () {  // Initialise LinkedList
+    Node[0] = ListNode("", 0); // Null values for Node[0] since index starts at 1, leaving this node unused.
+    for (int i = 1; i < 31; i++) {
+      Node[i] = ListNode("", i);
+    }
+    Start = 0;
+    NextFree = 1;
+  }
+
+  bool IsEmpty () {return Start == 0;}
+
+  void DisplayLinkedList () {
+    std::cout << "Start: " << Start << '\n';
+    std::cout << "NextFree: " << NextFree << '\n';
+    std::cout << "================================" << '\n';
+    std::cout << "Index | DataValue | PointerValue" << '\n';
+    for (int i = 1; i < 31; i++) {
+      std::cout << i << " | " << Node[i].GetDataValue() << " | " << Node[i].GetPointerValue() << '\n';
+    }
+  }
+
+
+  // Task 3.4
+};
